51ad1c50fe049ef04fcedd316dc5f381cc0b548b
Salah Adawi <sadawi@student.hive.fi>
2020-11-13 18:00:40 +0200

    add support for small and large heaps

--- malloc.c
+++ malloc.c
@@ -6,15 +6,34 @@
 /*   By: sadawi <sadawi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/29 13:58:17 by sadawi            #+#    #+#             */
-/*   Updated: 2020/11/13 17:09:05 by sadawi           ###   ########.fr       */
+/*   Updated: 2020/11/13 17:52:30 by sadawi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "malloc.h"
 
-void	align_on_bytes(size_t *num, size_t alignment)
+size_t	align_on_bytes(size_t num, size_t alignment)
 {
-	*num = (*num + (alignment - 1)) & ~(alignment - 1);
+	return ((num + (alignment - 1)) & ~(alignment - 1));
+}
+
+size_t	get_heap_size(size_t size)
+{
+	if (size <= (size_t)TINY_BLOCK_SIZE)
+		return (TINY_HEAP_ALLOCATION_SIZE);
+	if (size <= (size_t)SMALL_BLOCK_SIZE)
+		return (SMALL_HEAP_ALLOCATION_SIZE);
+	return (align_on_bytes(size, getpagesize()));
+}
+
+void	store_head(t_heap *new_heap, size_t size)
+{
+	if (size <= (size_t)TINY_BLOCK_SIZE)
+		g_malloc.tiny = new_heap;
+	else if (size <= (size_t)SMALL_BLOCK_SIZE)
+		g_malloc.small = new_heap;
+	else
+		g_malloc.large = new_heap;
 }
 
 t_heap	*create_new_heap(t_heap *prev, size_t size)
@@ -22,10 +41,7 @@ t_heap	*create_new_heap(t_heap *prev, size_t size)
 	t_heap	*new_heap;
 	size_t	heap_size;
 
-	//replace with different sized heaps
-	//if (size)
-	(void)size;
-		heap_size = TINY_HEAP_ALLOCATION_SIZE;
+	heap_size = get_heap_size(size);
 	new_heap = (t_heap*)mmap(NULL, heap_size, PROT_READ | PROT_WRITE,
 	MAP_PRIVATE | MAP_ANON, -1, 0);
 	new_heap->prev = prev;
@@ -35,21 +51,34 @@ t_heap	*create_new_heap(t_heap *prev, size_t size)
 	new_heap->size_available = heap_size - sizeof(t_heap);
 	if (prev)
 		prev->next = new_heap;
+	else
+		store_head(new_heap, size);
 	return (new_heap);
 }
 
-//inefficient, iterates twice if allocating new heap
+t_heap	*get_heap_list(size_t size)
+{
+	if (size <= (size_t)TINY_BLOCK_SIZE)
+		return (g_malloc.tiny);
+	if (size <= (size_t)SMALL_BLOCK_SIZE)
+		return (g_malloc.small);
+	return (g_malloc.large);
+}
+
 void	*get_heap(size_t size)
 {
 	t_heap *ptr;
 
-	ptr = g_malloc.tiny;
+	ptr = get_heap_list(size);
+	if (!ptr)
+		return (create_new_heap(ptr, size));
 	while (1)
 	{
-		if (!ptr || !ptr->next)
-			return (create_new_heap(ptr, size));
 		if (ptr->size_available >= size + sizeof(t_block))
 			return (ptr);
+		if (!ptr->next)
+			return (create_new_heap(ptr, size));
+		ptr = ptr->next;
 	}
 }
 
@@ -83,7 +112,7 @@ void	*get_block(t_heap *heap, size_t size)
 
 		}
 		prev = ptr;
-		ptr = ptr->next;
+		ptr = ptr->data_size + BLOCK_SHIFT(ptr);
 		i++;
 	}
 }
@@ -94,7 +123,7 @@ void	*malloc(size_t size)
 
 	if (size == 0)
 		return (NULL);
-	align_on_bytes(&size, 16);
+	size = align_on_bytes(size, 16);
 	ptr = get_heap(size);
 	ptr = get_block(ptr, size);
 	return (BLOCK_SHIFT(ptr));
