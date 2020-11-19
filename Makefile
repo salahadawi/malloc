dd3cf672cae54e5f79f329ab69b43290b61e7c6b
Salah Adawi <sadawi@student.hive.fi>
2020-11-19 19:05:59 +0200

    add functions to Makefile and header

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: sadawi <sadawi@student.hive.fi>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/04/10 19:26:38 by sadawi            #+#    #+#              #
-#    Updated: 2020/11/17 19:50:57 by sadawi           ###   ########.fr        #
+#    Updated: 2020/11/19 18:28:18 by sadawi           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,7 +17,7 @@ endif
 NAME = libft_malloc.so
 LINK = libft_malloc_$(HOSTTYPE).so
 
-CFILES = malloc.c free.c
+CFILES = malloc.c free.c realloc.c
 
 SRCS = $(addprefix srcs/, $(CFILES))
 OBJS = $(addprefix objs/, $(notdir $(SRCS:.c=.o)))
--- includes/malloc.h
+++ includes/malloc.h
@@ -6,7 +6,7 @@
 /*   By: sadawi <sadawi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/30 13:21:40 by sadawi            #+#    #+#             */
-/*   Updated: 2020/11/17 19:46:25 by sadawi           ###   ########.fr       */
+/*   Updated: 2020/11/19 18:29:17 by sadawi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,18 +40,11 @@ typedef struct		s_heap
 {
 	size_t			size;
 	size_t			block_amount;
+	size_t			blocks_freed;
 	struct s_heap	*prev;
 	struct s_heap	*next;
 
 }					t_heap;
-
-typedef struct		s_malloc
-{
-	t_heap			*tiny;
-	t_heap			*small;
-	t_heap			*large;
-
-}					t_malloc;
 
 typedef struct		s_heap_and_block
 {
@@ -60,6 +53,24 @@ typedef struct		s_heap_and_block
 
 }					t_heap_and_block;
 
+typedef struct		s_malloc
+{
+	t_heap			*tiny;
+	t_heap			*small;
+	t_heap			*large;
+
+	//tmp, probably not allowed
+	t_heap			*heap;
+	t_block			*block;
+
+}					t_malloc;
+
 t_malloc			g_malloc;
+
+void				show_alloc_mem(void);
+
+void				show_alloc_mem_string(void);
+
+int					find_block(t_heap *heap, void *ptr);
 
 #endif
